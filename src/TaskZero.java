76a95a5fe73611e8373fc67506b5bf79737a7be5
NikitaDmitruk <neomouz@icloud.com>
2024-03-29 22:29:53 +0300

    This code check the number is simple or not.

---
+++ src/TaskZero.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TaskZero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите простое целое число для задания размера массива: ");
+        if (scanner.hasNextInt()) {
+            int size = scanner.nextInt();
+            if ((size % 2 == 0 && size != 2) || size < 2) {
+                System.out.println("Вы ввели не простое число!");
+                return;
+            } else {
+                for (int i = 3; i <= Math.sqrt(size); i = i + 2) {
+                    if (size % i == 0) {
+                        System.out.println("Вы ввели не простое число!");
+                        return;
+                    }
+                }
+            }
+            System.out.println("Число " + size + " простое!");
+        } else System.out.println("Вы ввели не целое число!");
+    }
+}
