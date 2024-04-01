820db34b51b05b3cabe1ddd24ab310a46a3e0e9d
NikitaDmitruk <neomouz@icloud.com>
2024-04-01 17:39:21 +0300

    Добавил комментарии и изменил способ ввода массива данных.

--- src/TaskWithStar.java
+++ src/TaskWithStar.java
@@ -3,40 +3,39 @@ import java.util.Scanner;
 public class TaskWithStar {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите простое целое число для задания размера массива: ");
+        System.out.println("Введите целое положительное число для задания размера массива: ");
         if (scanner.hasNextInt()) {
             int size = scanner.nextInt();
-            if ((size % 2 == 0 && size != 2) || size < 2) {
-                System.out.println("Вы ввели не простое число!");
+            if (size < 1) {
+                System.out.println("Вы ввели 0 или отрицательное число!");
                 return;
-            } else {
-                for (int i = 3; i <= Math.sqrt(size); i = i + 2) {
-                    if (size % i == 0) {
-                        System.out.println("Вы ввели не простое число!");
-                        return;
-                    }
-                }
             }
-            System.out.println("Число " + size + " простое!");
             int[] randomNumbers = new int[size];
             System.out.println("Введите целые элементы массива размерностью " + size + ":");
             for (int i = 0; i < randomNumbers.length; i++) {
                 if (scanner.hasNextInt()) {
                     randomNumbers[i] = scanner.nextInt();
+                    if (randomNumbers[i] < 0) {
+                        System.out.println("Вы ввели отрицательное число!");
+                        return;
+                    }
                 } else {
                     System.out.println("Вы ввели не целое число!");
                     return;
                 }
             }
+            /* В этой части задаем начальное значение счетчика, который определяет в скольких элементах массива меняется
+            разряд числа, если мы прибавим к нему единицу */
             int count = 0;
             for (int i = 0; i < randomNumbers.length; i++) {
                 if ((randomNumbers[randomNumbers.length - i - 1] + 1) % 10 == 0) {
                     count = count + 1;
-                }
-                else {
+                } else {
                     break;
                 }
             }
+            /*  В этой части формируем массив с данными, в которых хранится разрядность чисел в массиве, которые сменили
+             разряд */
             int[] placeNumber = new int[count];
             int sizeNew = 0;
             for (int i = 0; i < count; i++) {
@@ -49,9 +48,11 @@ public class TaskWithStar {
                     placeNumber[placeNumber.length - 1 - i] = placeNumber[placeNumber.length - 1 - i] + 1;
                 }
             }
+            // На сколько нужно увеличить новый массив, для того, чтобы вписать туда 0
             for (int i = 0; i < count; i++) {
                 sizeNew = sizeNew + placeNumber[i];
             }
+            // Вывод результатов, в зависимости от исходной конструкции массива и данных, которые нужно изменить
             if (count == randomNumbers.length) {
                 int[] biggerRandomNumbers = new int[sizeNew + 1];
                 biggerRandomNumbers[0] = 1;
@@ -63,7 +64,7 @@ public class TaskWithStar {
                 }
             } else if (count == 0) {
                 randomNumbers[randomNumbers.length - 1] = randomNumbers[randomNumbers.length - 1] + 1;
-                for (int line:randomNumbers) {
+                for (int line : randomNumbers) {
                     System.out.println(line);
                 }
             } else {
